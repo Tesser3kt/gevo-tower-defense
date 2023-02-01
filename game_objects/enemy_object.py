84b8c912f4f32793855fd69f8978e36f32b04ed4
Jachym L <jachym.lowenhoffer@gmail.com>
2023-02-01 16:14:11 +0100

    added basic functionality for enemy class. also trying new branches

---
+++ game_objects/enemy_object.py
@@ -0,0 +1,24 @@
+from active_object import ActiveObject
+from mobile_object import MobileObject
+
+from pygame.sprite import Sprite
+from pygame.surface import Surface
+
+
+class Enemy_obejct(ActiveObject,MobileObject):
+    def __init__(self,x: int, y: int, width:int, height:int, image: Sprite, 
+    speed:int, direction:tuple[int],
+    animation:list[Surface], animation_index:int = 0
+    ) -> None:
+        ActiveObject.__init__(x,y,width,height,image,animation,animation_index)
+        MobileObject.__init__(x,y,width,height,image,speed,direction)
+    
+    def update(self)->None:
+        ActiveObject.update()
+        MobileObject.update()
+    
+    def find_path(self)->None:
+        #from current position and paths avalible determine
+        #in which direction should the enemy go
+        #change the direction in the object so the function returns None
+        pass
