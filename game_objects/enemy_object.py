06dabf37c0673776e9b1883460589135af3a50d7
Jachym L <jachym.lowenhoffer@gmail.com>
2023-02-01 18:19:09 +0100

    message

--- game_objects/enemy_object.py
+++ game_objects/enemy_object.py
@@ -5,22 +5,19 @@ from pygame.sprite import Sprite
 from pygame.surface import Surface
 
 
-class Enemy_obejct(ActiveObject,MobileObject):
-    def __init__(self, hp:int,
+class EnemyObejct(ActiveObject,MobileObject):
+    def __init__(self, hp:int, 
     x: int, y: int, width:int, height:int, image: Sprite, 
     speed:int, direction:tuple[int],
     animation:list[Surface], animation_index:int = 0,
+    detectable:bool=True,
     ) -> None:
         ActiveObject.__init__(x,y,width,height,image,animation,animation_index)
         MobileObject.__init__(x,y,width,height,image,speed,direction)
 
         self.hp=hp
+        self.detectable=detectable
+
     def update(self)->None:
         ActiveObject.update(self)
         MobileObject.update(self)
-    
-    def find_path(self)->None:
-        #from current position and paths avalible determine
-        #in which direction should the enemy go
-        #change the direction in the object so the function returns None
-        pass
