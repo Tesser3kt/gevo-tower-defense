fdd31c240facbde5b607731a13cc09a841582097
01000101l01110010l01101001l01100011 <eric.dusart.2005@gmail.com>
2023-02-01 18:49:48 +0100

    saving

---
+++ level_generator/level_generator_config.py
@@ -0,0 +1,19 @@
+"""Configuration for the level generator."""
+from config import Tile
+
+class Config_lvl_gen:
+    MAX_TURNING_POINTS = Tile.WIDTH//8
+    MIN_TURNING_POINTS = Tile.WIDTH//16
+
+    MAX_PROB = 1000
+    PROB_DECREASE = 400
+    MIN_STRAIGHT_LINE = 5
+
+    PATH_COLOR = (255, 255, 0) # Yellow
+
+    RANDOM_PROBS = {
+        "UP": 350,
+        "DOWN": 150,
+        "LEFT": 250,
+        "RIGHT": 250
+    }
