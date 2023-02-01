492474a82ce194e531a2adcc233891d6544dd1ea
Jachym L <jachym.lowenhoffer@gmail.com>
2023-02-01 20:01:54 +0100

    aded tower class and just skeleton for the functions. just the descriptions and variable

--- game_objects/active_object.py
+++ game_objects/active_object.py
@@ -5,20 +5,22 @@ class ActiveObject(GameObject):
     """ Class for objects that do something on their own """
 
     def __init__(
-        self, x:int, y:int, width:int, height:int, image:Surface, animation:list[Surface], animation_index:int = 0
+        self, x:int, y:int, width:int, height:int, image:Surface,
+         animation:list[Surface], animation_index:int = 0, lvl:int=1
         ) -> None:
 
         super().__init__(x,y,width,height,image)
         self.animation=animation
         self.animation_index=animation_index
+        self.lvl=lvl
     
     def update(self)->None:
         """update"""
         GameObject.update(self)
         self.continue_animation()
 
-    def continue_animation(self):
-        """ Moves the animation one frame ahead """
+    def continue_animation(self)->None:
+        """ Moves the animation index one frame ahead """
 
         self.animation_index+=1
         self.animation_index %= len(self.animation)
--- game_objects/enemies/enemy_object.py
+++ game_objects/enemies/enemy_object.py
@@ -1,18 +1,19 @@
 from active_object import ActiveObject
 from mobile_object import MobileObject
 
-from pygame.sprite import Sprite
 from pygame.surface import Surface
 
 
 class EnemyObejct(ActiveObject,MobileObject):
+    """ Most general class for enemy objects """
+
     def __init__(self, hp:int, 
-    x: int, y: int, width:int, height:int, image: Sprite, 
+    x: int, y: int, width:int, height:int, image: Surface, 
     speed:int, direction:tuple[int],
     animation:list[Surface], animation_index:int = 0,
-    detectable:bool=True,
+    detectable:bool=True,lvl:int=1
     ) -> None:
-        ActiveObject.__init__(x,y,width,height,image,animation,animation_index)
+        ActiveObject.__init__(x,y,width,height,image,animation,animation_index,lvl)
         MobileObject.__init__(x,y,width,height,image,speed,direction)
 
         self.hp=hp
