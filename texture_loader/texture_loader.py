fa12e7d0496ec51bb6ae4a2dd08b11e0230af682
Marek Koutny <marek.koutny@gevo.cz>
2023-02-09 15:36:59 +0100

    still working on texture_loader.py

--- texture_loader/texture_loader.py
+++ texture_loader/texture_loader.py
@@ -1,23 +1,25 @@
 """ texture loader module. """
-from config.general_config import *
+#from config.general_config import *
 from pygame.image import load
 import os
-
+BASE_DIR = r"C:\Users\HP\repa\gevo-tower-defense-1\texture_loader"
+ASSETS_DIR = "HP"
 
 class TextureLoader:
     """ Texture loader class. """
-    def __init__(self, BASE_DIR, ASSETS_DIR):
-        self.BASE_DIR = BASE_DIR
-        self.ASSETS_DIR = ASSETS_DIR
+    def __init__(self):
+        ...
 
     def load_all_textures(self) -> dict:
         """ Load all textures. """
-        assets_path = os.path.join(self.BASE_DIR, self.ASSETS_DIR)
+        #assets_path = os.path.join(BASE_DIR, ASSETS_DIR)
+        assets_path = r"C:\Users\HP\repa\gevo-tower-defense-1\texture_loader"
 
         texture_dict = {}
         for folder in os.listdir(assets_path):
             if folder not in texture_dict:
                 texture_dict[folder] = {}
+            
 
             for sub_folder in os.listdir(os.path.join(assets_path, folder)):
                 if sub_folder not in texture_dict[folder]:
@@ -31,3 +33,9 @@ class TextureLoader:
                         image = load(texture)
                         image = image.convert()
                         texture_dict[folder][sub_folder][textures].append(image)
+
+        return texture_dict
+        
+
+x = TextureLoader()
+print(x.load_all_textures())
