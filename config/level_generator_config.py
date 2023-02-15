310b433927ef249e325411b0f693e8236925893e
01000101l01110010l01101001l01100011 <eric.dusart.2005@gmail.com>
2023-02-15 17:21:56 +0100

    rozmístil jsem soubory do složek

---
+++ config/level_generator_config.py
@@ -0,0 +1,23 @@
+"""Configuration for the level generator."""
+from general_config import Tile
+
+class Config_lvl_gen:
+    MAX_TURNING_POINTS = Tile.WIDTH//8
+    MIN_TURNING_POINTS = Tile.WIDTH//16
+
+    MAX_PROB = 40000
+    PROB_DECREASE = 400
+    MIN_STRAIGHT_LINE = 5
+
+    PROB_NUMERATOR = 3
+    PROB_DIVISOR = 4
+
+
+    PATH_COLOR = (255, 255, 0) # Yellow
+
+    RANDOM_PROBS = {
+        "UP": 350,
+        "DOWN": 150,
+        "LEFT": 250,
+        "RIGHT": 250
+    }
