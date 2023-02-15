4484da32ec2b2bf19f1c3164d6fbf0ddd9a8fe24
Marek Koutny <marek.koutny@gevo.cz>
2023-02-15 18:21:22 +0100

    Added logging for texture_loader

--- texture_loader/texture_loader.py
+++ texture_loader/texture_loader.py
@@ -22,39 +22,55 @@ class TextureLoader:
         """ Load all textures. Returns dictionary of textures sorted by folders."""
         assets_path = os.path.join(BASE_DIR, ASSETS_DIR)
         logging.debug("Assets path created succesfully. ")
-        logging.debug(r"Function 'load_all_textures' of TextureLoader class object called succesfully.")
+        logging.info(r"Function 'load_all_textures' of TextureLoader class object called succesfully.")
         
 
         texture_dict = {}
         logging.debug("Empty dictionary created succesfully.")
+        if not os.path.exists(os.path.join(assets_path)):
+            logging.error(f"Path {assets_path} does not exist. Crashing game.")
+            print(f"Path {assets_path} does not exist.")
+            pg.quit()
         for folder in os.listdir(assets_path):
             logging.debug(f"Listing in folder{assets_path} ...")
             if folder not in texture_dict:
                 texture_dict[folder] = {}
                 logging.debug(f"Created empty dictionary for {folder}.")
             
-
+            if not os.path.exists(os.path.join(assets_path, folder)):
+                logging.error(f"Path {assets_path}\{folder} doesn't eist. ")
+                pg.quit()
             for sub_folder in os.listdir(os.path.join(assets_path, folder)):
                 logging.debug(f"Listing in {os.path.join(assets_path, folder)}...")
                 if sub_folder not in texture_dict[folder]:
                     texture_dict[folder][sub_folder] = {}
                     logging.debug(f"Created empty dictionary for {sub_folder}.")
-
+                
+                if not os.path.exists(os.path.join(assets_path, folder, sub_folder)):
+                    logging.error(f"Path {assets_path}\{folder}\{sub_folder} doesn't exist. ")
+                    pg.quit()
                 for textures in os.listdir(os.path.join(assets_path, folder, sub_folder)):
                     logging.debug(f"Listing in {os.path.join(assets_path, folder, sub_folder)}...")
                     if textures not in texture_dict[folder][sub_folder]:
                         texture_dict[folder][sub_folder][textures] = []
                         logging.debug(f"Created empty list for {textures}.")
 
+                    if not os.path.exists(os.path.join(assets_path, folder, sub_folder, textures)):
+                        logging.error(f"Path {os.path.join(assets_path, folder, sub_folder, textures)} doesn't exist.")
+                        pg.quit()
                     for texture in os.listdir(os.path.join(assets_path, folder, sub_folder, textures)):
-                        image = load(os.path.join(assets_path, folder, sub_folder, textures, texture))
-                        logging.debug(f"Loaded image {os.path.join(assets_path, folder, sub_folder, textures, texture)}")
-                        image = image.convert()
+                        try:
+                            image = load(os.path.join(assets_path, folder, sub_folder, textures, texture))
+                            logging.debug(f"Loaded image {os.path.join(assets_path, folder, sub_folder, textures, texture)}")
+                            image = image.convert()
+                        except Exception as e:
+                            logging.error(f"Failed to load or convert {texture}: {e.message}")
+                            pg.quit()
                         logging.debug(f"Converted image {os.path.join(assets_path, folder, sub_folder, textures, texture)}")
                         texture_dict[folder][sub_folder][textures].append(image)
                         logging.debug(f"Appended image {os.path.join(assets_path, folder, sub_folder, textures, texture)} to the list.")
 
-        logging.debug("Function 'load_all_textures' finished. ")
+        logging.info("Function 'load_all_textures' finished. ")
         return texture_dict
         
 # delete
