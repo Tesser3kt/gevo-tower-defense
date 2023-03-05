5058be6612800fb1cf00bfaab8aaa489ab643b42
01000101l01110010l01101001l01100011 <eric.dusart.2005@gmail.com>
2023-03-05 19:02:25 +0100

    added a menu for level difficulty: 0-5

--- level_generator/level_generator.py
+++ level_generator/level_generator.py
@@ -1,11 +1,5 @@
 """Avek sa kan dan la man"""
-
-from enum import Enum
-import random
-from PIL import Image
-
-from config.general_config import Tile, Colors
-from config.level_generator_config import *
+from level_generator.generator_imports import *
 
 class Direction(Enum):
     """Class that contains Direction vectors"""
