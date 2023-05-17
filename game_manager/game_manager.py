7c1ed34a6f33cf112978991e02c055f539928d61
Eric Dusart <eric.dusart.2005@gmail.com>
2023-05-17 21:05:04 +0200

    Bing chilling. Vlastně jsem něco udělal, ale pak smazal. Toto je ale úplný začátek game manageru, který někdy dokončím. Pro zlepšení dne doporučuji toto video: https://www.youtube.com/watch?v=sxT5bnowzcU

--- game_manager/game_manager.py
+++ game_manager/game_manager.py
@@ -1,5 +1,8 @@
 from pygame.sprite import RenderUpdates
 from pygame.time import Clock
+from pygame import event, QUIT, quit
+from pygame import locals
+
 from config.settings.general_config import Economy, Game
 
 
@@ -10,27 +13,43 @@ class GameManager:
         self.projectiles = RenderUpdates()
         self.towers = RenderUpdates()
         self.enemies = RenderUpdates()
+        self.effects = RenderUpdates()
 
         self.moving_objects = RenderUpdates()
         self.static_objects = RenderUpdates()
-
-
 
         self.ui = RenderUpdates()
 
         self.coins:int = Economy.STARTING_MONEY
         self.lives:int = Economy.STARTING_LIVES
         self.level:int = Game.START_LEVEL
-        self.wawe:int = Game.START_WAVE
-        self.effects = RenderUpdates()
+        self.wave:int = Game.START_WAVE
+
 
         self.clock = Clock()
-        self.running = True
-        self.paused = False
-        self.game_over = False
 
-    #   
+        self.running = True
+        self.pause = False
+
+
+    def handle_input(self):
+        for event in event.get():
+            if event.type == QUIT:
+                quit()
+            elif event.type == locals.K_ESCAPE:
+                self.pause = not self.pause
+
+    def update(self):
+        pass
+
     def run(self):
         while self.running:
             self.clock.tick(Game.FPS)
-            #TODO: finish the game loop, add pause, křížek
+
+            self.handle_input()
+
+            if self.pause:
+                continue
+
+    
+        
