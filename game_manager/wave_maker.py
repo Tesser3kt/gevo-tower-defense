8e315820499d9afdaf5330034c672b5b22028aa1
Eric Dusart <eric.dusart.2005@gmail.com>
2023-05-23 12:07:45 +0200

    created wave_maker

---
+++ game_manager/wave_maker.py
@@ -0,0 +1,42 @@
+""" Program that creates waves of enemies for the game."""
+from config.settings.general_config import *
+from game_manager.game_manager import GameManager
+from game_objects.enemies.enemy_object import EnemyObject
+from config.settings.enemies import *
+
+from random import choices
+
+def create_wave(wave_function:Wave_function , wave:int) -> list:
+    """ Create a wave of enemies"""
+    number_of_enemies = int(wave_function(wave))
+    enemies = []
+    if wave in Special_waves.boss_wave:
+        wave_difficulty = Spawn_probs.only_boss
+    elif wave in Special_waves.camo_wave:
+        wave_difficulty = Spawn_probs.only_camo
+    elif wave in Special_waves.speedrun_wave:
+        wave_difficulty = Spawn_probs.casual_speeder
+    elif wave in Special_waves.hard_wave:
+        wave_difficulty = Spawn_probs.hard
+    elif wave < 10:
+        wave_difficulty = Spawn_probs.only_casual
+    elif wave < 20:
+        wave_difficulty = Spawn_probs.casual_speeder
+    elif wave < 30:
+        wave_difficulty = Spawn_probs.normal
+    elif wave < 40:
+        wave_difficulty = Spawn_probs.hard
+    elif wave < 50:
+        wave_difficulty = Spawn_probs.harder
+    elif wave < 60:
+        wave_difficulty = Spawn_probs.impossible
+    
+    for i in range(number_of_enemies):
+        for enemy_type in wave_difficulty:
+            for j in range(int(number_of_enemies*wave_difficulty[enemy_type])):
+                enemies.append(enemy_type)
+
+    return choices(enemies, k=number_of_enemies)
+
+
+    
