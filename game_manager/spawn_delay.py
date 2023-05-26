a26e63d219134881c4cd6a554e03b26878262161
Eric Dusart <eric.dusart.2005@gmail.com>
2023-05-26 18:57:29 +0200

    fixed some errors

--- game_manager/spawn_delay.py
+++ game_manager/spawn_delay.py
@@ -4,4 +4,5 @@ def spawn_delay(enemy, the_enemy_after)-> int:
     """ Calculate spawn delay for the upcoming enemy  in frames"""
     return Game.FPS
 
+# Now it's one per second
 # TODO create AI system that choose a good delay for the upcoming enemy
